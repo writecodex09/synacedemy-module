455a57a3240c5a0b7db722694068859e0a035395
Kornelius Septa Jasa Sipayung <writecodex09@gmail.com>
2021-11-02 19:42:54 +0700

    add penjumlahan function

---
+++ calc.go
@@ -0,0 +1,8 @@
+package synacedemy_module
+
+import "fmt"
+
+func Penjumlahan(a, b int) {
+	hasil := a + b
+	fmt.Println(hasil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/writecodex09/synacedemy-module
+
+go 1.14
